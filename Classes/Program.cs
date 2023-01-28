dda52111aea48f536414797cb4a78a475028746d
Randall Smith <rdsmith1761@gmail.com>
2023-01-28 11:29:06 -0500

    Exercise1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var chevy = new Car();
+            var ford = new Car();
+
+            chevy.Make = "chevy";
+            chevy.Model = "Bolt";
+            chevy.Year = 2022;
+
+            ford.Make = "Ford";
+            ford.Model = "Mustang";
+            ford.Year = 2022;
+
+            Console.WriteLine("Make  Model   Year");
+            Console.WriteLine("------------------");
+
+            var cars = new List<Car>();
+            cars.Add(chevy);
+            cars.Add(ford);
+
+            foreach(var car in cars)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+            
+
+
+
+
         }
     }
 }
